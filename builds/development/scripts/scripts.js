c8cda1a005a9f943e509f1f373c75c42484ac296
Kristin Yetter <kristin@dameranchdesigns.com>
2017-04-01 08:53:22 -0700

    created modals

--- builds/development/scripts/scripts.js
+++ builds/development/scripts/scripts.js
@@ -24,116 +24,133 @@
                 width = $('#title').width();
                 $('.nav-elements').css('width', width + 'px');
             }
+            // trying to open details with jqueryUI widget
+            // $("#love-details").dialog({
+            //     appendTo: '#design',
+            //     autoOpen: false,
+            //     // height: 500,
+            //     width: 450,
+            //     modal: true,
+            //     cancel: function() {
+            //         $(this).dialog("close");
+            //         console.log('created cancel');
+            //     }
+            // });
+            // $("#love").click(function() {
+            //     $("#love-details").dialog("open");
+            //     console.log('opened modal');
+            //
+            // });
 
-            $("#love-details").dialog({
-            autoOpen: false,
-            height: 500,
-            width: 450,
-            modal: true,
-            cancel: function() {
-                $( this ).dialog( "close" );
-                console.log('created cancel');
-              }
+            $(".block").click(function() {
+              myobj = $(this);
+              console.log(myobj);
+              idName = myobj.attr("id");
+              id = '#'+idName + '-modal';
+              console.log(id)
+                $(id).appendTo("body").modal({fadeDuration: 300, fadeDelay: 1.5});
+                console.log("i clicked here");
+                return false;
             });
-               $( "#love" ).click(function() {
-                      $( "#love-details" ).dialog("open");
-                      console.log('opened modal');
-                   });
+
 
 
 
             /*on click toggle show hide detail element of blocks*/
-          //   $(".summary").click(function(e){
-          //     e.preventDefault();
-          //     var details=$(this).next('.details'),
-          //     dur = 700;
-          //     // details.toggleClass("transition, hidden");
-					// 		 details.slideToggle(dur, 'linear',  timeOut);
-           //
-          //      function timeOut() {
-          //        setTimeout(function(){
-          //          details.slideUp('fast');}, 1000000);
-          //        }
-          //  });
 
-          $('.accordion').accordion({
-            active: false,
-            heightStyle: "content",
-            header: "h3",
-            collapsible: true,
-            classes: {
-              "ui-accordion": "accordion-panel",
-              "ui-accordion-header": "accordion-header",
-              "ui-accordion-header-collapsed": "accordion-header-collapsed",
-              "ui-accordion-content": "accordion-content",
-              "ui-accordion-content-active": "accordion-content-active",
-              "ui-accordion-header-icon": "accordion-header-icon",
-              "ui-accordion-icons": "accordion-icons"},
-            animate: 300,
-              });
+            //   $(".summary").click(function(e){
 
-              // init controller
-              var controller = new ScrollMagic.Controller({
-              });
+            //     e.preventDefault();
 
-              //calculate window height - height of #title-logo
-              var winHeight = $(window).height() - 230;
+            //     var details= $(this).next('.details'),
+            //     dur = 700;
+            //     // details.toggleClass("transition, hidden");
+            // 		 details.slideToggle(dur, 'linear',  timeOut);
+            //
+            //      function timeOut() {
+            //        setTimeout(function(){
+            //          details.slideUp('fast');}, 1000000);
+            //        }
+            //  });
 
-              //tweenMax timeline for logo rotate, scale and bounce out
-              var tweenLogo = new TimelineMax()
-              .add(TweenMax.to('#title-logo', .5, {
-                scale: 2,
-                rotation: 180,
-                ease:Back.easeOut
-              }))
-              .add(TweenMax.to('#title-logo', 1, {
-                y: winHeight,
-                x: -20,
-                ease: Bounce.easeOut
-              }))
-              .add(TweenMax.to('#title-logo',.5, {
-                opacity: 0,
-                scale: 0
-              }));
+            $('.accordion').accordion({
+                active: false,
+                heightStyle: "content",
+                header: "h3",
+                collapsible: true,
+                classes: {
+                    "ui-accordion": "accordion-panel",
+                    "ui-accordion-header": "accordion-header",
+                    "ui-accordion-header-collapsed": "accordion-header-collapsed",
+                    "ui-accordion-content": "accordion-content",
+                    "ui-accordion-content-active": "accordion-content-active",
+                    "ui-accordion-header-icon": "accordion-header-icon",
+                    "ui-accordion-icons": "accordion-icons"
+                },
+                animate: 300,
+            });
 
-              //new scrollScene for the #title-logo animation
-          var sceneLogo = new ScrollMagic.Scene({
-            triggerElement: '#pic1',
-            triggerHook: '.9'
-          })
-          .setTween(tweenLogo)
-          .addIndicators()
-          .addTo(controller);
+            // init controller
+            var controller = new ScrollMagic.Controller({});
 
-              //pins the pic backgrounds and let's next section move up on the picture
-              $(".pic-background").each(function() {
+            //calculate window height - height of #title-logo
+            var winHeight = $(window).height() - 230;
+
+            //tweenMax timeline for logo rotate, scale and bounce out
+            var tweenLogo = new TimelineMax()
+                .add(TweenMax.to('#title-logo', .5, {
+                    scale: 2,
+                    rotation: 180,
+                    ease: Back.easeOut
+                }))
+                .add(TweenMax.to('#title-logo', 1, {
+                    y: winHeight,
+                    x: -20,
+                    ease: Bounce.easeOut
+                }))
+                .add(TweenMax.to('#title-logo', .5, {
+                    opacity: 0,
+                    scale: 0
+                }));
+
+            //new scrollScene for the #title-logo animation
+            var sceneLogo = new ScrollMagic.Scene({
+                    triggerElement: '#pic1',
+                    triggerHook: '.9'
+                })
+                .setTween(tweenLogo)
+                // .addIndicators()
+                .addTo(controller);
+
+            //pins the pic backgrounds and let's next section move up on the picture
+            $(".pic-background").each(function() {
                 new ScrollMagic.Scene({
-                  triggerElement: this,
-                  triggerHook: '0',
-                  duration: 800
-                  })
-                  .setPin(this, {
-                    pushFollowers: false
-                  })
-                  .addIndicators()
-                  .addTo(controller);
-
-                });
-
-                //pins the content sections so that they stay in place for a bit of extra scrolling
-                $(".content").each(function() {
-                  new ScrollMagic.Scene({
-                    triggerElement: this,
-                    triggerHook: '0',
-                    duration: 300
+                        triggerElement: this,
+                        triggerHook: '0',
+                        duration: 800
                     })
                     .setPin(this, {
-                      pushFollowers: true
+                        pushFollowers: false
                     })
-                    .addIndicators()
+                    // .addIndicators()
                     .addTo(controller);
 
-                  });
+            });
+
+            //pins the content sections so that they stay in place for a bit of extra scrolling
+            $(".content").each(function() {
+                new ScrollMagic.Scene({
+                        triggerElement: this,
+                        triggerHook: '0',
+                        duration: 300
+                    })
+                    .setPin(this, {
+                        pushFollowers: true
+                    })
+                    // .addIndicators()
+                    .addTo(controller);
+
+            });
 
 
 
